1d8f43e9d64b612540e61817c019449560150528
can <sxcuican@hotmail.com>
2018-08-01 10:08:42 +0800

    Added: docs

--- src/Stubs/route.stub.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$graphConfig = config('graph.');
-
-Route::any($graphConfig['routePrefix'] . ':action','\smilecc\think\GraphQLController@action')
-    ->middleware(\smilecc\think\GraphQLMiddleware::class);
